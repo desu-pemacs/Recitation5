26ec3ab91b79c6dc24da6d74b5d9ecf22f198f78
mrasamny <mrasamny@desu.edu>
2021-10-19 06:57:42 -0400

    initial commit

--- src/main/java/Recitation3.java
+++ src/main/java/Recitation3.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Recitation5 {
+public class Recitation3 {
     public static void main(String[] argv)
     {
         Scanner scan = new Scanner(System.in);
